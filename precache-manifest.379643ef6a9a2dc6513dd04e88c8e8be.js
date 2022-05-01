ab32e9744c48383b5166a06c45c80d8e55175521
ananta-raj-shrestha <eranantarajshrestha@gmail.com>
2022-05-01 19:20:56 +0545

    Final reactjs-text-analyzer-project

---
+++ precache-manifest.379643ef6a9a2dc6513dd04e88c8e8be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6090d844e3ac3a00f5e",
+    "url": "/static/css/main.a8f2d64b.chunk.css"
+  },
+  {
+    "revision": "e6090d844e3ac3a00f5e",
+    "url": "/static/js/main.e6090d84.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "d2949add91e9a2d97ae7",
+    "url": "/static/js/2.d2949add.chunk.js"
+  },
+  {
+    "revision": "7ee0e2da555c3fd35366",
+    "url": "/static/js/runtime~main.7ee0e2da.js"
+  },
+  {
+    "revision": "e607451d21cfe26205d882bc9a2e27c6",
+    "url": "/index.html"
+  }
+];
